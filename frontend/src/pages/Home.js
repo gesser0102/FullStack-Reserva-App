3ab4d7226fc28531de31961ae100518ac1b549bb
Rodrigo Gesser <gesserrodrigo@hotmail.com>
2023-06-16 20:49:54 -0300

    final

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
       <section id="Home" className="py-32 px-3 bg-amber-300 min-h-screen 0 sm:flex sm:justify-center sm:items-center">
         <div className="bg-gray-700 px-8 rounded-2xl py-5 lg:max-w-4xl mx-auto min-h-[24rem] justify-content gap-6 lg:flex-row flex-col-reverse items-center">
           <div className="flex flex-col justify-center items-center w-full">
-          <img src="./images/loeffa.png" alt="" className="p-2" />
+          <img src="./images/logo.png" width="200px" alt="" className="p-2" />
             <h2 className="text-2xl font-semibold text-white mt-10">
               Aplicativo de Reservas
             </h2>
